591b69dbda95316a339a6e7f6c3cfb60999518d7
songming05 <45953864+songming05@users.noreply.github.com>
2022-10-05 20:06:52 +0900

    Config: initial commit

---
+++ src/main/java/stripe/song/coupon/web/CouponController.java
@@ -0,0 +1,32 @@
+package stripe.song.coupon.web;
+
+
+import org.springframework.web.bind.annotation.*;
+
+public class CouponController {
+
+
+    //RESTful API
+//    @GetMapping
+//    //HTTP GET 조회 POST 생성/저장 PUT 수정 DEL 삭제
+//    @PostMapping
+//    @PutMapping
+//    @DeleteMapping
+
+//        @RequestParam  >> query param
+//        @RequestBody
+        // Object
+
+
+        //Test --> service
+
+        //junit 5 public 생략, DisplayName ~~~~
+
+        //assertThat()
+
+//        stripe.name.pay.web
+//                service
+//                repoistory
+
+
+}
