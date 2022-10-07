3de429c800881b371e4188f720f10985b8c39e86
zilzu <zilzu4165@naver.com>
2022-10-08 00:09:39 +0900

    feature : retrieveCoupon 구현

--- src/main/java/stripe/song/coupon/web/CouponController.java
+++ src/main/java/stripe/song/coupon/web/CouponController.java
@@ -3,30 +3,10 @@ package stripe.song.coupon.web;
 
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class CouponController {
-
-
-    //RESTful API
-//    @GetMapping
-//    //HTTP GET 조회 POST 생성/저장 PUT 수정 DEL 삭제
-//    @PostMapping
-//    @PutMapping
-//    @DeleteMapping
-
-//        @RequestParam  >> query param
-//        @RequestBody
-        // Object
-
-
-        //Test --> service
-
-        //junit 5 public 생략, DisplayName ~~~~
-
-        //assertThat()
-
-//        stripe.name.pay.web
-//                service
-//                repoistory
-
-
+    @GetMapping("api/v1/coupons/{id}")
+    public String retrieveCoupon(@PathVariable String id) {
+        return id + " : Z4OV52SU";
+    }
 }
