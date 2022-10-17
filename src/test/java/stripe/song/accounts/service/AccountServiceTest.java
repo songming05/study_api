eb67f068af0bc77904e903310de6838a630e87e7
songming05 <45953864+songming05@users.noreply.github.com>
2022-10-18 01:14:59 +0900

    Test: 계정 생성 기능 단위 테스트

---
+++ src/test/java/stripe/song/accounts/service/AccountServiceTest.java
@@ -0,0 +1,21 @@
+package stripe.song.accounts.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import stripe.song.accounts.common.Account;
+import stripe.song.accounts.repository.AccountRepository;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    @DisplayName("계정을 신규로 생성한다")
+    void create_account_by_new_id() {
+        AccountRepository accountRepository = new AccountRepository();
+        AccountService accountService = new AccountService(accountRepository);
+        Account newAccount = accountService.create();
+        assertThat(newAccount).isNotNull();
+        assertThat(newAccount.accountId.value).isEqualTo(accountRepository.retreiveById(newAccount.accountId).accountId.value);
+    }
+}
