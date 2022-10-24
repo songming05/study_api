a44e856a8e170f6fe001754c098a2cb46b242d07
Jun_Water <tnwnsrla@naver.com>
2022-10-24 11:14:09 +0900

    Feature : Customer/Delete 테스트

--- src/test/java/stripe/jun/customer/repository/CustomerRepositoryTest.java
+++ src/test/java/stripe/jun/customer/repository/CustomerRepositoryTest.java
@@ -1,19 +1,11 @@
 package stripe.jun.customer.repository;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import stripe.jun.customer.service.Customer;
-import stripe.jun.customer.service.CustomerCreationRequest;
-import stripe.jun.customer.service.CustomerIdGenerator;
-import stripe.jun.customer.service.CustomerService;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import stripe.jun.customer.common.CustomerId;
+import stripe.jun.customer.service.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -24,15 +16,15 @@ class CustomerRepositoryTest {
     @Autowired
     CustomerService customerService;
 
-    String customerId = new CustomerIdGenerator().generateCustomerId();
+    String generateCustomerId = new CustomerIdGenerator().generateCustomerId();
 
     @Test
     @DisplayName("Customer Id 생성 테스트")
     void customerIdGeneratorTest() {
 
-        assertThat(customerId.length()).isEqualTo(18); /** CustomerId 는 18자리 이어야 한다. */
-        assertThat(customerId.substring(0, 4)).isEqualTo("cus_"); /** CustomerId 의 앞 4글자는 cus_ 고정이다. */
-        assertThat(customerId.substring(4)).containsPattern("[a-zA-Z0-9]*"); /** CustomerId 의 5번째는 영어소문자, 대문자, 숫자식으로 이루어져 있어야 한다.*/
+        assertThat(generateCustomerId.length()).isEqualTo(18);
+        assertThat(generateCustomerId.substring(0, 4)).isEqualTo("cus_");
+        assertThat(generateCustomerId.substring(4)).containsPattern("[a-zA-Z0-9]*");
     }
 
     @Test
@@ -42,15 +34,32 @@ class CustomerRepositoryTest {
         CustomerCreationRequest customerCreationRequest = new CustomerCreationRequest("김준수"
                 , "010-1234-5678"
                 , "부자가 되고 싶은 사람");
-        Customer customer = customerService.create(customerCreationRequest); /** CustomerId 저장 */
+        Customer customer = customerService.create(customerCreationRequest);
 
         //When
-        Customer customerRetrieve = customerService.retrieve(customer.customerId); /** CustomerId 찾기 */
+        Customer customerRetrieve = customerService.retrieve(customer.customerId);
 
         //Then
-        assertThat(customer.customerId).isEqualTo(customerRetrieve.customerId); /** 저장한 Id 와 찾는 Id가 같다.*/
-        assertThatThrownBy(() -> customerService.retrieve("1234")).isInstanceOf(IllegalArgumentException.class); /** 저장한 적 없는 Id(1234)를 조회하였을 때, IllegalArgumentException 를 반환한다.  */
+        assertThat(customer.customerId).isEqualTo(customerRetrieve.customerId);
+        assertThatThrownBy(() -> customerService.retrieve("1234")).isInstanceOf(IllegalArgumentException.class);
+    }
 
+    @Test
+    @DisplayName("Customer delete 테스트, 존재하는 Id 를 삭제하면 remove(return value)가 되고, 존재하지 않는다면 메시지를 반환한다.")
+    void customerDeleteTest() {
+        //Given
+        CustomerCreationRequest customerCreationRequest = new CustomerCreationRequest("김준수"
+                , "010-1234-5678"
+                , "부자가 되고 싶은 사람");
+        Customer customer = customerService.create(customerCreationRequest);
+        CustomerId customerId = new CustomerId(customer.customerId);
+        CustomerId customerIdForException = new CustomerId(generateCustomerId);
 
+        //When
+        CustomerDeleteResult customerDeleteResult = customerService.delete(customerId);
+
+        //Then
+        assertThat(customer.customerId).isEqualTo(customerDeleteResult.customerId);
+        assertThatThrownBy(() -> customerService.delete(customerIdForException)).isInstanceOf(IllegalArgumentException.class);
     }
 }
